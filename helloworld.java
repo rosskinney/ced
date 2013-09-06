f831ff810e39870324a77750737410df5e191408
Ross Kinney <railspup@gmail.com>
2013-09-06 14:49:59 -0600

    Commit first Jave program for Udacity course

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+//My first Java program
+//Written September 6, 2013
+//First homework assignment Udacity....
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("World!");
+    }
+}

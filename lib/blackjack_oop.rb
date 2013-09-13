5f20ede347dc73ab90b3df6bb0f3bc52e46bff9a
Ross Kinney <railspup@gmail.com>
2013-09-12 21:19:15 -0600

    Commit additional changes to blackjack.oop

--- lib/blackjack_oop.rb
+++ lib/blackjack_oop.rb
@@ -7,6 +7,11 @@
 require 'rubygems'
 require 'pry'
 
+# Object Oriented Blackjack game
+
+# 1) Abstraction
+# 2) Encapsulation
+
 class Card
   attr_accessor :suit, :face_value
 
@@ -16,7 +21,7 @@ class Card
   end
 
   def pretty_output
-    puts "The #{face_value} of #{find_suit}"
+    "The #{face_value} of #{find_suit}"
   end
 
   def to_s
@@ -60,16 +65,85 @@ class Deck
   end
 end
 
+module Hand
+  def show_hand
+    puts "---- #{name}'s Hand ----"
+    cards.each do|card|
+      puts "=> #{card}"
+    end
+    puts "=> Total: #{total}"
+  end
+
+  def total
+    face_values = cards.map{|card| card.face_value }
+
+    total = 0
+    face_values.each do |val|
+      if val == "A"
+        total += 11
+      else
+        total += (val.to_i == 0 ? 10 : val.to_i)
+      end
+    end
+
+    #correct for Aces
+    face_values.select{|val| val == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def add_card(new_card)
+    cards << new_card
+  end
+
+  def is_busted?
+    total > 21
+  end
+end
+
 class Player
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize(n)
+    @name = n
+    @cards = []
+  end
+
 end
 
 class Dealer
+  include Hand
+
+  attr_accessor :name, :cards
+
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end
 end
 
-class Hand
-end
+
 
 
 deck = Deck.new
+
+player = Player.new("Chris")
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.add_card(deck.deal_one)
+player.show_hand
+
+dealer = Dealer.new
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.add_card(deck.deal_one)
+dealer.show_hand
 
 

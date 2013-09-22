07a0b9e13a2895450eb45c4c5a5dba0d44f59115
Ross Kinney <railspup@gmail.com>
2013-09-22 00:17:55 -0600

    Commit lambda program to + - * /

---
+++ lib/lambda2.rb
@@ -0,0 +1,7 @@
+Addition = lambda {|a, b| return a + b } # use this as your example!
+
+Subtraction = lambda {|a, b| return a - b } # your code between the braces
+
+Multiplication = lambda {|a, b| return a * b } # your code between the braces
+
+Division = lambda {|a, b| return a / b } # your code between the braces

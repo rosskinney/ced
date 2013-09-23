6fb20f19943a7dea39a9db458ac2429c7ee13580
Ross Kinney <railspup@gmail.com>
2013-09-22 20:58:24 -0600

    Add first php program

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>Hello, World!</title>
+	</head>
+	<body>
+        <!-- Add your PHP code in the tag below! -->
+        <h1><?php
+              echo "I'm learning PHP!"; 
+            ?></h1>
+	</body>
+</html>
